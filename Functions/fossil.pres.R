1d51745c880c8f978c15c018364770d2aec07878
calebowski <cnscutt1@sheffield.ac.uk>
2025-04-17 18:18:29 +0100

    continuous traits

--- Functions/fossil.pres.R
+++ Functions/fossil.pres.R
@@ -1,10 +1,12 @@
 ## function to create trees of only extant species
-remove.fossil <- function(trees, matrices) {
+remove.fossil <- function(trees, matrices, type = c("discrete", "continuous")) {
   living_matrices <- Map(function(tree, matrix) {
     ages <- tree.age(tree) # get tip ages
     tips <- ages$element[ages$ages == 0] # find only living species
     living_matrix <- matrix[rownames(matrix) %in% tips, ]
+    if (type == "discrete"){
     living_matrix <- apply(living_matrix, c(1, 2), as.character)
+    }
  # keep only living species in matrix
     living_tree <- keep.tip(tree, tips) # keep only living species in tree
     return(list(matrix = living_matrix, tree = living_tree)) # return as list
@@ -14,7 +16,7 @@ remove.fossil <- function(trees, matrices) {
 }
 
 ## function to remove fossils at varying preservation levels
-fossil.pres <- function(trees, matrices, preservation = c(0.05, 0.15, 0.5)){
+fossil.pres <- function(trees, matrices, preservation = c(0.05, 0.15, 0.5), type = c("discrete", "continuous")){
   if (!preservation %in% c(0.05, 0.15, 0.5)) {
        stop("Invalid preservation value. Must be one of 0.05, 0.15, or 0.5.")
    }
@@ -32,8 +34,10 @@ fossil.pres <- function(trees, matrices, preservation = c(0.05, 0.15, 0.5)){
         sample <- sample(fossils, size = ceiling(length(fossils) * 0.05))
         }
     kept <- c(sample, tips)
-    fossil_matrix <- matrix[rownames(matrix) %in% kept, ] # keep taxa that are fossil preserved
-    fossil_matrix <- apply(fossil_matrix, c(1, 2), as.character) # make character for ace
+    fossil_matrix <- matrix[rownames(matrix) %in% kept, ]
+    if (type == "discrete") { # keep taxa that are fossil preserved
+    fossil_matrix <- apply(fossil_matrix, c(1, 2), as.character)
+    } # make character for discrete ace
     pruned  <- keep.tip(tree, kept) # prune tree
     return(list(matrix = fossil_matrix, tree = pruned))
    }, trees, matrices)
