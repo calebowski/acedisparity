d84564246a1a649599edc3ef1e08d8c09cbe6b5d
calebowski <cnscutt1@sheffield.ac.uk>
2025-04-18 14:38:43 +0100

    binning fossil pres

--- Functions/fossil.pres.R
+++ Functions/fossil.pres.R
@@ -2,13 +2,14 @@
 remove.fossil <- function(trees, matrices, type = c("discrete", "continuous")) {
   living_matrices <- Map(function(tree, matrix) {
     ages <- tree.age(tree) # get tip ages
-    tips <- ages$element[ages$ages == 0] # find only living species
-    living_matrix <- matrix[rownames(matrix) %in% tips, ]
+    tips <- ages$element[ages$ages == 0]# find only living species
+    # root <- tree$node.label[1] 
+    living_matrix <- matrix[rownames(matrix) %in% c(tips, "t1"), ]
     if (type == "discrete"){
     living_matrix <- apply(living_matrix, c(1, 2), as.character)
     }
  # keep only living species in matrix
-    living_tree <- keep.tip(tree, tips) # keep only living species in tree
+    living_tree <- keep.tip(tree, c(tips, "t1")) # keep only living species in tree
     return(list(matrix = living_matrix, tree = living_tree)) # return as list
   }, trees, matrices)
   
@@ -16,24 +17,34 @@ remove.fossil <- function(trees, matrices, type = c("discrete", "continuous")) {
 }
 
 ## function to remove fossils at varying preservation levels
+## idea - split time into time bins and vary preservation level per bin
 fossil.pres <- function(trees, matrices, preservation = c(0.05, 0.15, 0.5), type = c("discrete", "continuous")){
   if (!preservation %in% c(0.05, 0.15, 0.5)) {
        stop("Invalid preservation value. Must be one of 0.05, 0.15, or 0.5.")
    }
    fossil_matrices <- Map(function(tree, matrix){
     ages <- tree.age(tree)
-    tips <- ages$element[ages$ages == 0] # keep living species
-    fossils <- ages$element[ages$ages > 0 & grepl("^t", ages$element)]  # find fossils
-    if(preservation == 0.5){
-        sample <- sample(fossils, size = length(fossils)/2)
-        } ## sample fossils by a specified preservation level
-    if(preservation == 0.15){
-        sample <- sample(fossils, size = ceiling(length(fossils) * 0.15))
-        }
-    if(preservation == 0.05){
-        sample <- sample(fossils, size = ceiling(length(fossils) * 0.05))
-        }
-    kept <- c(sample, tips)
+    tips <- ages$element[ages$ages == 0]     # keep living species
+
+    ## create time bins
+    range <- range(ages$ages)
+    bins <- seq(range[1], range[2], length.out = 4)
+    bin_ids <- cut(ages$ages, breaks = bins, labels = FALSE, include.lowest = TRUE)
+    ages$bin <- bin_ids
+    # root <- tree$node.label[1]
+
+    fossilb1 <- ages$element[ages$ages > 0 & grepl("^t", ages$element) & ages$bin == 1]
+    fossilb2 <- ages$element[ages$ages > 0 & grepl("^t", ages$element) & ages$bin == 2]
+    fossilb3 <- ages$element[ages$ages > 0 & grepl("^t", ages$element) & ages$bin == 3]
+    fossilb4 <- ages$element[ages$ages > 0 & grepl("^t", ages$element) & ages$bin == 4]
+    fossilb5 <- ages$element[ages$ages > 0 & grepl("^t", ages$element) & ages$bin == 5]
+    fossils <- list(bin1 = fossilb1, bin2 = fossilb2, bin3 = fossilb3, bin4 = fossilb4, bin5 = fossilb5)  # find fossils
+
+    sample <- lapply(fossils, function(bin) {
+      sample(bin, size = length(bin) * preservation)
+    })
+
+    kept <- c(unlist(unname(sample)), tips, "t1")
     fossil_matrix <- matrix[rownames(matrix) %in% kept, ]
     if (type == "discrete") { # keep taxa that are fossil preserved
     fossil_matrix <- apply(fossil_matrix, c(1, 2), as.character)
