2d9a767eb13c6d2cd467e18c50bd3081bd2e8044
calebowski <cnscutt1@sheffield.ac.uk>
2026-04-20 13:19:00 +0100

    cleanup repo

--- Analyses/trees/histogram.R
+++ Analyses/trees/histogram.R
@@ -1,9 +1,9 @@
-metadata <- read.csv("../Data/trees/metadata/metadata_all.csv")
+metadata <- read.csv("Data/trees/metadata/metadata_all.csv")
 
 metadata$net_div <- metadata$speciation - metadata$extinction
 
 
-pdf("../Manuscript/draft/figures/net_div_hist_overall_trees.pdf")
+pdf("Manuscript/draft/figures/net_div_hist_overall_trees.pdf")
 hist(metadata$net_div, 
      xaxp = c(0, 1, 10), 
      main = "", 
@@ -19,8 +19,8 @@ fossil_trees <- list()
 for (size in tree_sizes) {
   fossil_trees[[size]] <- list()
     for(i in 1:100) {
-      file_path <- file.path("..", "Data", "trees", 
-                            sprintf("fossil_trees_%st_%03d.rds", size, i))
+      file_path <- file.path("Data", "trees", 
+                            sprintf("fossil_trees_%s_%03d.rds", size, i))
       if(file.exists(file_path)) {
         fossil_trees[[size]][[i]] <- readRDS(file_path)
       } else {
@@ -33,13 +33,25 @@ for (size in tree_sizes) {
 
 colless_indices_2 <- lapply(fossil_trees, lapply, lapply, lapply, colless_corr)
 
+sampling_levels <- c("all", "fossil_high", "fossil_med", "fossil_low", "living")
+level_titles <- c("all" = "100%", "fossil_high" = "50%", "fossil_med" = "15%", "fossil_low" = "5%", "living" = "0%")
 
+pdf("Manuscript/draft/figures/symmetry.pdf", width = 15, height = 9)
+par(mfrow = c(length(tree_sizes), length(sampling_levels)), mar = c(4, 4, 3, 1))
 
-
-
-
-
-pdf("../Manuscript/draft/figures/symmetry.pdf")
-par(mfrow = c(3,2))
-hist(unlist(colless_indices_2))
+for (size in tree_sizes) {
+  for (level in sampling_levels) {
+    vals <- unlist(lapply(colless_indices_2[[size]], function(rep) {
+      rep[[1]][[level]]
+    }))
+    
+    hist(vals, 
+          main = paste(size, "-", level_titles[level]), 
+          xlab = "Corrected Colless Index",
+          # col = "grey80", border = "white",
+          cex.main = 1.8,  
+          cex.lab  = 1.6, 
+          cex.axis = 1.4)
+  }
+}
 dev.off()
